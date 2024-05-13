573f7bf29da94111205113f5f1c2e6d1f29d7d7f
Kirbisz <164274721+Kirbisz@users.noreply.github.com>
2024-05-13 23:17:28 +0000

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Repositório de código frontend de formulário de caastro.
 <h2>Sistemas de Informação</h2>
 <h3>Programação Front-end</h3>
+# Estudos_de_PY
---
+++ Untitled-1.py
@@ -0,0 +1,49 @@
+# i = 1 
+
+# while i < 10:
+#    print(i)
+#   i +=1
+
+
+#   print("terminou")
+#  print(i)
+
+#pokemons = ['pikachu', 'eve' , 'bulbasauro', 'charmander' , 'squirtle']
+
+# for item in pokemons:
+#   print(item)
+
+#  sapato = "allstar"
+# for letra in sapato:
+ #   print(letra)
+
+
+# for index in range(len(pokemons)):
+  #  print(pokemons[index],index)
+
+
+#for index in range(5):
+#    if index == 0:
+#       print("primeira linha")
+#   else:
+#        print(f"outras linhas {index}")
+
+
+matrix_numeros = [
+    [1,2,3],
+    [4,5,6],
+   [7,8,9],
+    [0],
+]
+
+for linha in matrix_numeros:
+  print(linha)
+  print("----")
+  for coluna in linha:
+      print(coluna)
+
+
+
+
+
+
